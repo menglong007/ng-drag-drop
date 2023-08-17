dd6776bd11357703ffe8413526a2737db9c191db
Your Name <you@example.com>
2023-08-17 15:39:15 +0700

    drag&drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import {Component} from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls:['./app.component.css']
 })
 export class AppComponent {
   date: any[] = [];
@@ -14,16 +16,20 @@ export class AppComponent {
         "months": [
           {
             "name": 'january',
-            "products": ['P1', 'P2', 'P3']
+            "products": ['P1', 'P2', 'P3','P4','P5','P6']
           },
           {
             "name": 'feb',
-            "products": ['P1', 'P2']
+            "products": ['P1', 'P2', 'P3','P4','P5','P6']
           },
           {
             "name": 'mar',
-            "products": ['P1', 'P2']
-          }
+            "products": ['P1', 'P2', 'P3','P4','P5','P6']
+          },
+          {
+          "name": 'april',
+          "products": ['P1', 'P2', 'P3','P4','P5','P6']
+          },
         ]
       },
       {
@@ -31,18 +37,57 @@ export class AppComponent {
         "months": [
           {
             "name": 'january',
-            "products": ['P1', 'P2', 'P3']
+            "products": ['P1', 'P2', 'P3','P4','P5','P6']
           },
           {
             "name": 'feb',
-            "products": ['P1', 'P2']
+            "products": ['P1', 'P2', 'P3','P4','P5','P6']
           },
           {
             "name": 'mar',
-            "products": ['P1', 'P2']
-          }
+            "products": ['P1', 'P2', 'P3','P4','P5','P6']
+          },
+          {
+            "name": 'april',
+            "products": ['P1', 'P2', 'P3','P4','P5','P6']
+          },
         ]
       },
+      {
+        "name": "TM03",
+        "months": [
+          {
+            "name": 'january',
+            "products": ['P1', 'P2', 'P3','P4','P5','P6']
+          },
+          {
+            "name": 'feb',
+            "products": ['P1', 'P2', 'P3','P4','P5','P6']
+          },
+          {
+            "name": 'mar',
+            "products": ['P1', 'P2', 'P3','P4','P5','P6']
+          },
+          {
+            "name": 'april',
+            "products": ['P1', 'P2', 'P3','P4','P5','P6']
+          },
+        ]
+      },
+
     ]
+  }
+
+  drop(event: CdkDragDrop<string[]> , products : any[] ) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatListModule} from "@angular/material/list";
+import {CdkDrag, CdkDropList, CdkDropListGroup} from "@angular/cdk/drag-drop";
 
 @NgModule({
   declarations: [
@@ -11,7 +12,10 @@ import {MatListModule} from "@angular/material/list";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatListModule
+    MatListModule,
+    CdkDropList,
+    CdkDrag,
+    CdkDropListGroup
   ],
   providers: [],
   bootstrap: [AppComponent]
